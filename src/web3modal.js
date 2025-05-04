1b210b4fc449a9f4dd88a0e5c4e8bc18fd6537d0
Stefan <dutchdelphidude@gmail.com>
2025-05-04 17:59:07 +0200

    introducing TAppKit.Open() and TAppKit.Disconnect()

--- src/web3modal.js
+++ src/web3modal.js
@@ -1,28 +1,31 @@
 window.appKit = {
-    create  : null,
-    arbitrum: null,
-    mainnet : null,
-    optimism: null,
-    polygon : null,
-    sepolia : null
+  create  : null,
+  arbitrum: null,
+  base    : null,
+  mainnet : null,
+  optimism: null,
+  polygon : null,
+  sepolia : null
 }
 
 import { createAppKit } from '@reown/appkit';
 import { EthersAdapter } from '@reown/appkit-adapter-ethers';
-import { arbitrum, mainnet, optimism, polygon, sepolia } from '@reown/appkit/networks';
+import { arbitrum, base, mainnet, optimism, polygon, sepolia } from '@reown/appkit/networks';
 
-window.appKit.create = (networks, projectId, analytics) => {
-    return createAppKit({
-        adapters: [new EthersAdapter()],
-        networks,
-        projectId,
-        features: {
-            analytics,
-        },
-    })
+window.appKit.create = (networks, projectId, darkMode, analytics) => {
+  return createAppKit({
+    adapters: [new EthersAdapter()],
+    networks,
+    projectId,
+    themeMode: darkMode ? 'dark' : 'light',
+    features: {
+      analytics,
+    },
+  })
 }
 
 window.appKit.arbitrum = arbitrum;
+window.appKit.base     = base;
 window.appKit.mainnet  = mainnet;
 window.appKit.optimism = optimism;
 window.appKit.polygon  = polygon;
--- web3modal.pas
+++ web3modal.pas
@@ -16,38 +16,89 @@ unit web3modal;
 interface
 
 uses
+  // Delphi
+  System.SysUtils,
+  // pas2js
   JS;
 
 const
   ProjectId = 'b56e18d47c72ab683b10814fe9495694'; // this is a public projectId only to use on localhost
 
 type
+  TView    = (Connect, Networks, Account);
+  TOptions = TJSObject;
+
+  TAccount = class external name 'Object'(TJSObject)
+  strict protected
+    FAddress  : string;  external name 'address';
+    FConnected: Boolean; external name 'isConnected';
+    FStatus   : string;  external name 'status';
+  public
+    property Address  : string  read FAddress;
+    property Connected: Boolean read FConnected;
+    property Status   : string  read FStatus;
+  end;
+
+  TState = class external name 'Object'(TJSObject)
+  strict private
+    FLoading          : Boolean; external name 'loading';
+    FOpen             : Boolean; external name 'open';
+    FSelectedNetworkId: string;  external name 'selectedNetworkId';
+    FActiveChain      : string;  external name 'activeChain';
+    FInitialized      : Boolean; external name 'initialized';
+  public
+    property Loading          : Boolean read FLoading;
+    property Open             : Boolean read FOpen;
+    property SelectedNetworkId: string  read FSelectedNetworkId;
+    property ActiveChain      : string  read FActiveChain;
+    property Initialized      : Boolean read FInitialized;
+  end;
+
   TAppKit = class external name 'Object'(TJSObject)
   public
-    function Connected: Boolean; external name 'getIsConnectedState';
+    function  Connected: Boolean;                                external name 'getIsConnectedState';
+    procedure Open; async;                                       external name 'open'; overload;
+    procedure Open(const options: TOptions); async;              external name 'open'; overload;
+    procedure Disconnect; async;                                 external name 'disconnect';
+    procedure SubscribeAccount(const callback: TProc<TAccount>); external name 'subscribeAccount';
+    procedure SubscribeState(const callback: TProc<TState>);     external name 'subscribeState';
   end;
 
   TNetwork = class external name 'Object'(TJSObject)
   strict private
-    FName: string; external name 'name';
     FId  : UInt64; external name 'id';
+    FName: string; external name 'name';
   public
-    property Name: string read FName;
     property Id  : UInt64 read FId;
+    property Name: string read FName;
   end;
 
 function CreateAppKit(
   const networks : TArray<TNetwork>;
   const projectId: string;
+  const darkMode : Boolean;
   const analytics: Boolean): TAppKit; external name 'window.appKit.create';
 
 const
   Arbitrum: TNetwork; external name 'window.appKit.arbitrum';
+  Base    : TNetwork; external name 'window.appKit.base';
   Mainnet : TNetwork; external name 'window.appKit.mainnet';
   Optimism: TNetwork; external name 'window.appKit.optimism';
   Polygon : TNetwork; external name 'window.appKit.polygon';
   Sepolia : TNetwork; external name 'window.appKit.sepolia';
 
+function Options(const view: TView): TOptions;
+
 implementation
+
+function Options(const view: TView): TOptions;
+begin
+  Result := TOptions.New;
+  case view of
+    Connect : Result['view'] := 'Connect';
+    Networks: Result['view'] := 'Networks';
+    Account : Result['view'] := 'Account';
+  end;
+end;
 
 end.
